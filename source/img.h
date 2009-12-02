34a620e773562de93167c69c12f6bdf3f821fa58
Buntoids <zayfox@live.com.au>
2009-12-02 12:13:50 +1000

    Base Source

---
+++ source/img.h
@@ -0,0 +1,30 @@
+#ifndef _IMG_
+#define _IMG_
+
+#include "shared.h"
+
+#include <SDL/SDL.h>
+
+#include <map>
+
+class Image {
+	private:
+		SDL_Surface *surface;
+
+	public:
+		Image():surface(NULL){}
+		void free();
+		void load(int);
+		void load(std::string);
+		void draw(const Sint16,const Sint16);
+		void draw_static(const Sint16,const Sint16);
+};
+
+extern std::map<int,Image> imglist;
+extern std::vector<Image> poseimg;
+extern std::vector<Image> battleimg;
+
+void loadimage(const int&);
+
+#endif
+
